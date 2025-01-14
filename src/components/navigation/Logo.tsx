2fb0cee4cf9620326f8fcf21d50d21f3411b7e73
armanbiz <armanmkhitaryanbiz@gmail.com>
2025-01-15 00:06:34 +0100

    Updated Logo

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Brain } from 'lucide-react';
 
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <Brain className="h-6 w-6 text-blue-500" />
+      <img src="/Klavix_logo.svg" alt="Klavix Logo" className="h-8 w-auto" />
       <span className="text-xl tech-heading gradient-text">KLAVIX</span>
     </div>
   );
