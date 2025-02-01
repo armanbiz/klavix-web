eda52c2862e814e9d55e8ebdd4527daff1041662
null <null>
2025-02-01 21:19:05 +0100

    again fixed Logo.tsx file to make logo visible

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <img src="/Klavix_logo.svg" alt="Klavix Logo" className="h-8 w-auto" />
+      <img src="/Klavix_logo.svg" alt="Klavix Logo" className="h-8 w-8" />
       <span className="text-xl tech-heading gradient-text">KLAVIX</span>
     </div>
   );
