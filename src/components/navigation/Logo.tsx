35fcb088e12d13c0e962372e786ad591b26e8a38
armanbiz <armanmkhitaryanbiz@gmail.com>
2025-03-13 23:24:18 +0100

    removed logo from website for now

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <img 
-        src="/Klavix_logo_New.png"
-        alt="Klavix Logo" 
-        className="h-8 w-8"
-      />
       <span className="text-xl tech-heading gradient-text">KLAVIX</span>
     </div>
   );
